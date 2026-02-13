38da567969636e5e012689bd5bb24dc1fad8640d
4mh23cs018-design <4mh23cs018@gmail.com>
2026-02-13 12:25:42 +0530

    usdy

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
